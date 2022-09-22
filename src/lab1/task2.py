b0d09e457f276f9dc5c02c52f9d3212e2d2b989b
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-09-22 12:34:56 +0200

    feat: safe apply function

--- src/lab1/task2.py
+++ src/lab1/task2.py
@@ -1,12 +1,30 @@
 import operator
 import argparse
+import math
+from typing import Callable
+
+
+def apply_function_safely(function: Callable, arguments: list, unpack: bool = False):
+    try:
+        return function(*arguments) if unpack else function(arguments) 
+    except TypeError:
+        return False
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("operation", type=str)
-    parser.add_argument("number_1", type=int)
-    parser.add_argument("number_2", type=int)
+    parser.add_argument("arguments", nargs="+", type=int)
     args = parser.parse_args()
-    apply_fuction = getattr(operator, args.operation)
-    print(apply_fuction(args.number_1, args.number_2))
+    module = None    
+    if hasattr(operator, args.operation):
+        module = operator
+    elif hasattr(math, args.operation):
+        module = math
+
+    if module:
+        function = getattr(module, args.operation)
+        result = apply_function_safely(function, args.arguments) or apply_function_safely(function, args.arguments, unpack=True)
+    else:
+        print(f"Module with function '{args.operation}' is not found.")
+    print(result)
