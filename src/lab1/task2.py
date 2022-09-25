2797dcf018d7cfc67815636432e6399bbf2f3ef0
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-09-25 20:07:26 +0200

    refactor: task 2

--- src/lab1/task2.py
+++ src/lab1/task2.py
@@ -5,6 +5,7 @@ from typing import Callable
 
 
 def apply_function_safely(function: Callable, arguments: list, unpack: bool = False):
+    """Returns false if function raised error else returns result"""
     try:
         return function(*arguments) if unpack else function(arguments) 
     except TypeError:
@@ -16,15 +17,16 @@ if __name__ == "__main__":
     parser.add_argument("operation", type=str)
     parser.add_argument("arguments", nargs="+", type=int)
     args = parser.parse_args()
-    module = None    
-    if hasattr(operator, args.operation):
-        module = operator
-    elif hasattr(math, args.operation):
-        module = math
 
-    if module:
-        function = getattr(module, args.operation)
-        result = apply_function_safely(function, args.arguments) or apply_function_safely(function, args.arguments, unpack=True)
+    modules = [operator, math]
+    function = None
+    for module in modules:
+        if hasattr(module, args.operation):
+            function = getattr(module, args.operation)
+            break
     else:
         print(f"Module with function '{args.operation}' is not found.")
+        
+    if function: 
+        result = apply_function_safely(function, args.arguments) or apply_function_safely(function, args.arguments, unpack=True)
     print(result)
