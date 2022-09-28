7cafe8e635683ae20b170cf7729a31f7752b25f4
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-09-28 20:52:48 +0200

    feat: add FileAnalyzer

--- src/lab2/task4.py
+++ src/lab2/task4.py
@@ -1,32 +1,49 @@
 from functools import cached_property
-from typing import Tuple
-from collections import Counter
-import nltk
+from typing import Any
 import re
 
 
-class FileAnalyzer:
+class FileAnalyzer(dict):
     def __init__(self, file_path: str) -> None:
         self._file_path = file_path
-        self._words_freq = self.count_words(self.file_content)
-    
+        self.stats = {}
+        self.stats["n_characters"] = self._count_characters(self.file_content)
+        self.stats["n_words"] = self._count_words(self.file_content)
+        self.stats["n_sentences"] = self._count_sentences(self.file_content)
+
+    def print_stats(self) -> None:
+        print(f"File {self._file_path} statistics:\n")
+        for key, value in self.stats.items():
+            print(f"\t{key}: {value}")
+
     @cached_property
     def file_content(self) -> str:
         with open(self._file_path, "r") as file:
             return file.read()
     
-    def get_content(self) -> dict:
-        return dict(self._words_freq)
+    def _count_characters(self, content: str) -> int:
+        if not isinstance(content, str):
+            raise TypeError()
+        return len(content)
+    
+    def _count_words(self, content: str) -> int:
+        if not isinstance(content, str):
+            raise TypeError()
+        content = content.lower()
+        content = re.sub(r'[^\w\s]', '', content)
+        words = content.split(" ")
+        return len(words)
 
-    def count_words(self, content: str) -> dict:
-        """Returns: counts top 10."""
-        content = content.strip()
-        words = nltk.word_tokenize(content)
-        top_10_characters = Counter(words)
-        return top_10_characters
+    def _count_sentences(self, content: str) -> int:
+        if not isinstance(content, str):
+            raise TypeError()
+        sentences = re.split(r'[.!?]+', content)
+        return len(sentences)
+
+    def __getitem__(self, key: Any) -> Any:
+        return self.stats[key]
 
 
 if __name__ == "__main__":
-    nltk.download("punkt")
     file_analyzer = FileAnalyzer("src/lab2/test.txt")
-    print(file_analyzer.get_content())
+    file_analyzer.print_stats()
